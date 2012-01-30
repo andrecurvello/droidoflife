a4258eedb1d711fed1b7d133b4ee7ceaada103b2
chrulri <chrulri@gmail.com>
2012-01-30 19:48:23 +0100

    implement about dialog

--- src/com/chrulri/droidoflife/AboutDialogFragment.java
+++ src/com/chrulri/droidoflife/AboutDialogFragment.java
@@ -17,17 +17,43 @@
  ******************************************************************************/
 package com.chrulri.droidoflife;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class AboutDialogFragment extends DialogFragment {
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.about, container, false);
-	}
+  private CharSequence infoText;
+  private CharSequence legalText;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setStyle(STYLE_NO_TITLE, 0);
+    String version = "";
+    try {
+      PackageInfo packageInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+      version = packageInfo.versionName;
+    } catch (PackageManager.NameNotFoundException e) {
+      // nop
+    }
+    infoText = Html.fromHtml(getString(R.string.about_info, version));
+    legalText = Html.fromHtml(getString(R.string.about_legal));
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    View v = inflater.inflate(R.layout.about, container, false);
+    TextView info = (TextView) v.findViewById(R.id.about_info);
+    info.setText(infoText);
+    TextView legal = (TextView) v.findViewById(R.id.about_legal);
+    legal.setText(legalText);
+    return v;
+  }
 }
